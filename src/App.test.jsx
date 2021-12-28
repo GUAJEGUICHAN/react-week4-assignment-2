e082e640c10ed37a3d8d9d1c5375444a88e3b40b
GUAJEGUICHAN <qkr135qkr@gmail.com>
2021-12-28 14:06:33 +0900

    [test] App 테스트 코드 / 통과

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,9 +1,19 @@
 import { render } from '@testing-library/react';
+import { useSelector } from 'react-redux';
 
 import App from './App';
 
-jest.mock('react-redux');
+
+jest.mock('react-redux')
 test('App', () => {
+  useSelector.mockImplementation((selector) => ({
+    restaurants: [{
+      id: 1,
+      name: '김밥제국',
+      category: '분식',
+      address: '서울시 강남구 역삼동 ',
+    }]
+  }))
   const { getByText } = render((
     <App />
   ));
