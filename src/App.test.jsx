91cfd06efd08125a6d463cdbbfe87ea4c2799e08
GUAJEGUICHAN <qkr135qkr@gmail.com>
2021-12-28 13:01:08 +0900

    [refactor] Restaurants 컴포넌트 생성 / 테스트 통과 유지

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,5 +1,4 @@
 import { render } from '@testing-library/react';
-import { useSelector } from 'react-redux';
 
 import App from './App';
 
@@ -7,6 +6,6 @@ jest.mock('react-redux');
 test('App', () => {
   const { getByText } = render((
     <App />
-  ))
-  expect(getByText(/김밥제국/)).not.toBeNull()
+  ));
+  expect(getByText(/김밥제국/)).not.toBeNull();
 });
