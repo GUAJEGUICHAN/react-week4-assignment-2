707245619fc465e4a13ec19a2d35bd97c927c7c2
GUAJEGUICHAN <qkr135qkr@gmail.com>
2021-12-28 14:38:39 +0900

    [feature] App.jsx에서 dispatch기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,12 @@ import restaurants from './fixtures/restaurants';
 import RestaurantsContainer from './RestaurantsContainer';
 
 export default function App() {
+  const dispatch = useDispatch()
+  // TODO: 초기 실행
+  // restaurants에 데이터 넣기 => dispatch
+  useEffect(() => {
+    dispatch(setRestaurants(restaurants));
+  }, [])
   return (
     <div>
       <h1>Restaurants</h1>
