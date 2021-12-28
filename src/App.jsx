a5598022b2255f2840254ca26a36829d35e37820
GUAJEGUICHAN <qkr135qkr@gmail.com>
2021-12-28 13:43:04 +0900

    [refactor] 관심사 분리 - 데이터를 다루는 부분을 분리한다.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
+import restaurants from './fixtures/restaurants';
 import Restaurants from './Restaurants';
 
 export default function App() {
-  const restaurants = [{
-    id: 1,
-    name: '김밥제국',
-    category: '분식',
-    address: '서울시 강남구 역삼동 '
-  }]
-
   return (
     <div>
       <h1>Restaurants</h1>
--- src/Restaurants.test.jsx
+++ src/Restaurants.test.jsx
@@ -1,21 +1,15 @@
-//우리가 Restaurants.jsx에 무엇을 쓸 것인가를 적자.
-//App.test.jsx에서 가져와서 사용해보자
+// 우리가 Restaurants.jsx에 무엇을 쓸 것인가를 적자.
+// App.test.jsx에서 가져와서 사용해보자
 
 import { render } from '@testing-library/react';
+import restaurants from './fixtures/restaurants';
 
 import Restaurants from './Restaurants';
 
 test('Restaurants', () => {
-  const restaurants = [{
-    id: 1,
-    name: '김밥제국',
-    category: '분식',
-    address: '서울시 강남구 역삼동 '
-  }]
+
   const { getByText } = render((
     <Restaurants restaurants={restaurants} />
   ));
   expect(getByText(/김밥제국/)).not.toBeNull();
 });
-
-
