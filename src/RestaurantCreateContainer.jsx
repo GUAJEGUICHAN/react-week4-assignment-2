0d0132aecf30857b0cb9d57407990efafb14ff51
GUAJEGUICHAN <qkr135qkr@gmail.com>
2021-12-29 11:57:20 +0900

    [feature] RestaurantCreateContainer 구현

---
+++ src/RestaurantCreateContainer.jsx
@@ -0,0 +1,21 @@
+import { useDispatch, useSelector } from 'react-redux';
+import RestaurantForm from './RestaurantForm';
+
+import { changeRestaurantField } from './actions';
+
+export default function RestaurantCreateContainer() {
+  const { restaurant } = useSelector((state) => ({
+    restaurant: state.restaurant,
+  }));
+
+  const dispatch = useDispatch();
+  function handleChange(name, value) {
+    dispatch(changeRestaurantField(name, value));
+  }
+
+  return (
+    <div>
+      <RestaurantForm restaurant={restaurant} onChange={handleChange} />
+    </div>
+  );
+}
